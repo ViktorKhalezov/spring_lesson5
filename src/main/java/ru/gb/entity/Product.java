962b64a37377a153934a82108ad74014cdd588a7
Viktor Khalezov <viktor-khalezov@mail.ru>
2022-02-06 01:07:34 +0300

    Домашнее задание урок№5

--- src/main/java/ru/gb/entity/Product.java
+++ src/main/java/ru/gb/entity/Product.java
@@ -3,6 +3,7 @@ package ru.gb.entity;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -12,9 +13,34 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Builder
 @ToString
+@Entity
+@Table(name = "PRODUCT")
+@NamedQueries({
+    @NamedQuery(name = "Product.FindById",
+    query = "SELECT p FROM Product p WHERE p.id = :id"),
+        @NamedQuery(name = "Product.FindTitleById",
+                query = "SELECT p.title FROM Product p WHERE p.id = :id")
+})
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
     private Long id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "cost")
     private BigDecimal cost;
+
+    @Column(name = "manufacture_date")
     private LocalDate date;
+
+    @Column(name = "manufacturer_id")
+    private Long manufacturerId;
+
+    @Transient
+    private String manfacturerName;
+
+
 }
